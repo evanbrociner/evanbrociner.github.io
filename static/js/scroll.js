087565f27d10fb1296ef577ea48369954c0b4e1e
evanbrociner <evanbrociner@gmail.com>
2024-05-24 14:45:58 -0400

    better navigation

---
+++ static/js/scroll.js
@@ -0,0 +1,10 @@
+window.onscroll = function () {
+  updateProgressBar();
+};
+
+function updateProgressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
