fab8d3fa33b0ebee2be835ada83010f18f24b511
evanbrociner <evanbrociner@gmail.com>
2024-05-25 15:36:17 -0400

    improved UX of VBC

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -8,3 +8,25 @@ function updateProgressBar() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("progressBar").style.width = scrolled + "%";
 }
+
+// Get all list items
+const listItems = document.querySelectorAll(".list-group-item");
+
+// Add click event listener to each item
+listItems.forEach(item => {
+  item.addEventListener("click", event => {
+    event.preventDefault(); // Prevent default link behavior
+
+    // Get the target section ID from the href attribute
+    const targetId = item.getAttribute("href");
+
+    // Scroll to the target section smoothly
+    document.querySelector(targetId).scrollIntoView({behavior: "smooth"});
+  });
+});
+
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: "#navbar",
+  offset: 100,
+  method: "auto" // or 'offset' for more precise scrolling
+});
